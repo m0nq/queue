6beb52835b26315b10219f43f3e59426234894f5
Monk <monq.wellington@gmail.com>
2016-03-29 22:13:30 -0700

    create working angular skeleton

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.use(express.static(__dirname + '/public'));
 ////////////////////////
 // ROUTE/REGISTRATION //
 ////////////////////////
+// a unique path identifier for partials
+app.get('/partials/:partialPath', function (req, res) {
+  res.render('partials/' + req.params.partialPath);
+});
 // default match all routes; i.e. catch all in order to coordinate/not conflict with Angular routing
 app.get('*', function (req, res) {
   res.render('index');
