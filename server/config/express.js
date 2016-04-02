24cc6bfa69ebfa9c5ce44cdacdca1c8aeb8fd0bb
Monk <monq.wellington@gmail.com>
2016-04-02 13:20:23 -0700

    update cookie parser and session middleware

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,9 @@
 var express = require('express'),
     stylus = require('stylus'),
+    passport = require('passport'),
     bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
     logger = require('morgan'),
     methodOverride = require('method-override');
 
@@ -18,6 +21,16 @@ module.exports = function (app, config) {
 
   // colored console logging
   app.use(logger('dev'));
+
+  // passport middleware setup with express cookie parser and sessions
+  app.use(cookieParser());
+  app.use(session({
+    secret: 'multi vision unicorns',
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // parse application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: false }));
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,3 +1,4 @@
+var passport = require('passport');
 ////////////////////////
 // ROUTE/REGISTRATION //
 ////////////////////////
@@ -6,6 +7,21 @@ module.exports = function (app) {
   app.get('/partials/:partialPath', function (req, res) {
     res.render('partials/' + req.params.partialPath);
   });
+
+  // post route when a user is successfully found in the db
+  app.post('/login', function (req, res, next) {
+    var auth = passport.authenticate('local', function (err, user) {
+      if (err) {return next(err);}
+      if (!user) {res.send({success: false});}
+      // logIn is a function that passport adds to the req object
+      req.logIn(user, function (err) {
+        if (err) {return next(err);}
+        res.send({success: true, user: user});
+      });
+    })
+    auth(req, res, next);
+  });
+
   // default match all routes; i.e. catch all in order to coordinate/not conflict with Angular routing
   app.get('*', function (req, res) {
     res.render('index');
