9e9fa43645da0da1cf2300c4b73f6c6612d6d6ca
Monk <monq.wellington@gmail.com>
2016-04-05 17:00:31 -0700

    create authorization access for api

--- server/config/auth.js
+++ server/config/auth.js
@@ -21,3 +21,14 @@ exports.requiresApiLogin = function (req, res, next) {
     next();
   }
 };
+
+exports.requiresRole = function (role) {
+  return function (req, res, next) {
+    if (!req.isAuthenticated() || req.user.roles.indexOf(role) === -1) {
+      res.status(403);
+      res.end();
+    } else {
+      next();
+    }
+  };
+};
--- server/config/routes.js
+++ server/config/routes.js
@@ -7,7 +7,7 @@ var auth = require('./auth'),
 ////////////////////////
 module.exports = function (app) {
 
-  app.get('/api/users', auth.requiresApiLogin, function (req, res) {
+  app.get('/api/users', auth.requiresRole('admin'), function (req, res) {
     User.find({}).exec(function (err, collection) {
       res.send(collection);
     });
